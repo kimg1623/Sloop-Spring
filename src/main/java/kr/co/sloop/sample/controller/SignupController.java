0cd1603940d2c39e73ab6c827ffffea9f5c5d4d8
kidchang93 <lck930721@gmail.com>
2023-12-28 17:03:23 +0900

    add : signupForm.jsp 추가

--- src/main/java/kr/co/sloop/sample/controller/SignupController.java
+++ src/main/java/kr/co/sloop/sample/controller/SignupController.java
@@ -2,13 +2,18 @@ package kr.co.sloop.sample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-
 public class SignupController {
 
     @GetMapping("/signup")
     public String signupForm(){
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(){
+        
     }
 }
