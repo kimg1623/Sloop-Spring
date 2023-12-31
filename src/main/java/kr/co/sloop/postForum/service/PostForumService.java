19c32d0897b3c88130e1c5beee4569fb36670ad2
thegreatjy <thegreatjy@gmail.com>
2023-12-31 21:14:53 +0900

    Revert "Docs(feature/postForum): ckeditor4 라이브러리 추가"
    This reverts commit d16db0e01f14b5c25e88bc871589e3ac1e7ead0f.

--- src/main/java/kr/co/sloop/postForum/service/PostForumService.java
+++
@@ -1,7 +0,0 @@
-package kr.co.sloop.postForum.service;
-
-import kr.co.sloop.postForum.domain.PostForumDTO;
-
-public interface PostForumService {
-    public boolean write(PostForumDTO postForumDTO);
-}
