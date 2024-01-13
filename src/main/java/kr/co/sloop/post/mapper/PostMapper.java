979eb8eef1ea613b806073976d1ca18659d5fbe2
thegreatjy <thegreatjy@gmail.com>
2024-01-14 02:31:27 +0900

    Refactor(feature/postAssignment): post DB 테이블에 글 삽입하기

--- src/main/java/kr/co/sloop/post/mapper/PostMapper.java
+++ src/main/java/kr/co/sloop/post/mapper/PostMapper.java
@@ -1,9 +1,12 @@
 package kr.co.sloop.post.mapper;
 
+import kr.co.sloop.post.domain.PostDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PostMapper {
 
     int countAllPostsByBoardIdx(int boardIdx);
+
+    int insertPost(PostDTO postDTO);
 }
--- src/main/java/kr/co/sloop/post/repository/PostRepository.java
+++ src/main/java/kr/co/sloop/post/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package kr.co.sloop.post.repository;
 
+import kr.co.sloop.post.domain.PostDTO;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository {
     int countAllPostsByBoardIdx(int boardIdx);
+    // post 테이블에 글 작성하기
+    int insertPost(PostDTO postDTO);
 }
