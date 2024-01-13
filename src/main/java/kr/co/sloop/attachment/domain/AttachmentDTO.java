548b6cbe9b323eecbecf4597f838a03771e595f3
thegreatjy <thegreatjy@gmail.com>
2024-01-14 02:27:11 +0900

    Add(feature/postAssignment): 첨부파일 객체 생성자 추가

--- src/main/java/kr/co/sloop/attachment/domain/AttachmentDTO.java
+++ src/main/java/kr/co/sloop/attachment/domain/AttachmentDTO.java
@@ -3,7 +3,11 @@ package kr.co.sloop.attachment.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+// 첨부파일 객체
 @Getter
 @Setter
 @ToString
@@ -14,4 +18,14 @@ public class AttachmentDTO {
     private String attachmentDirPath;   // 저장 디렉터리 경로
     private String attachmentName;  // 파일명
     private String attachmentThumbnail; // 썸네일 이미지 파일명
+
+    private MultipartFile multipartFile; // 실제 첨부파일
+
+    // 생성자 (게시글 idx, 원래 파일명, 저장 디렉터리 경로, 파일명)
+    public AttachmentDTO(int postIdx, String attachmentOrgName, String attachmentDirPath, String attachmentName){
+        this.postIdx = postIdx;
+        this.attachmentOrgName = attachmentOrgName;
+        this.attachmentDirPath = attachmentDirPath;
+        this.attachmentName = attachmentName;
+    }
 }
